8cf9c67aa1457efd088676c5843b08373874ba29
sbrezo <brezovac.sasa@gmail.com>
2023-12-04 10:19:58 +0100

    Initial Commit

---
+++ RouterPatternNavigation_App_SwiftUI/RouterPatternNavigation_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterPatternNavigation_App_SwiftUIApp.swift
+//  RouterPatternNavigation_App_SwiftUI
+//
+//  Created by Saša Brezovac on 04.12.2023..
+//
+
+import SwiftUI
+
+@main
+struct RouterPatternNavigation_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RouterPatternNavigation_App_SwiftUIUITests/RouterPatternNavigation_App_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RouterPatternNavigation_App_SwiftUIUITestsLaunchTests.swift
+//  RouterPatternNavigation_App_SwiftUIUITests
+//
+//  Created by Saša Brezovac on 04.12.2023..
+//
+
+import XCTest
+
+final class RouterPatternNavigation_App_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
